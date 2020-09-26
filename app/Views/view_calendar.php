9eb6217f0bcfb21b9972c4f960f8a23ea79d2b99
davedjoh <ivandidjohgah@gmail.com>
2020-09-26 19:28:15 +0700

    FRONTEND - Refactor Codebase-1

--- app/Views/view_calendar.php
+++ app/Views/view_calendar.php
@@ -9,7 +9,7 @@ navbar_before_login($nav_title);
 <div class="container">
     <!-- start content here -->
     <div class="col-lg-12" style="float:none;margin:auto;">
-        <iframe src="http://localhost/dist/" style="position: fixed; padding-top:55px; left:0;bottom:0; right: 0; width: 100%; height: 100%; border: none; margin:0; overflow-x: hidden; z-index:1;">
+        <iframe src="http://localhost/rapat/" style="position: fixed; padding-top:55px; left:0;bottom:0; right: 0; width: 100%; height: 100%; border: none; margin:0; overflow-x: hidden; z-index:1;">
             Your browser doesn't support iframes
         </iframe>
     </div>
